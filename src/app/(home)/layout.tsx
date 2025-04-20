45310118678f43974068f9d82a246dd1a34e453d
Thxn <155278099+thanchayawikgithub@users.noreply.github.com>
2025-04-20 14:06:00 +0700

    03-home-layout

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Navbar } from "@/components/layout";
+import Footer from "@/components/layout/footer";
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-1 bg-[#f4f4f0]">{children}</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
